5c48b785ba5e4ed2be41dad587979bf616ded388
Cevik <cev@hotmail.de>
2024-05-26 21:19:51 +0200

    add logic to the greetings
    handle cases in which greetingCount == || < 0
    
    concatenate greetings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(greeting, greetingCount) {
+    if (greetingCount < 0){
+        return "ERROR"
+    }
+    let greetingNew = greeting;
+    if (greetingCount == 0)
+        {
+            greetingNew = "";
+        }
+    else if (greetingCount > 0){
+        for (let i = 1; i < greetingCount; i++){
+            greetingNew = greetingNew.concat(greeting);
+        }
+    }
+    // console.log(greetingNew);
+    return greetingNew;
 };
 
 // Do not edit below this line
