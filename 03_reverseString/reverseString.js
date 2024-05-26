2042a81ba78646d7895075d3d5d4e29b7d1ebead
Cevik <cev@hotmail.de>
2024-05-26 21:27:37 +0200

    return reversed input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let reversedString = inputString.split('').reverse().join('');
+    return reversedString;
 };
 
 // Do not edit below this line
