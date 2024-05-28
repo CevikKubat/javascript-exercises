e19d43c20b0c039d352258981cc09727ef90dafe
Cevik <cev@hotmail.de>
2024-05-28 19:53:07 +0200

    solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...targetElements) {
+    let newArray = []; // Initialize newArray as an empty array
+    for (let i = 0; i < array.length; i++) {
+        if (!targetElements.includes(array[i])) {
+            newArray.push(array[i]); // Add element to newArray if it's not in targetElements
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
