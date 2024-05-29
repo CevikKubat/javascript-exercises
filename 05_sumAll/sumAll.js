78ee716cea9d9dcdb6c946e6439f0cfe158f3886
Cevik <cev@hotmail.de>
2024-05-29 22:41:51 +0200

    add logic for if larger number gets put first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
 const sumAll = function(smaller, bigger) {
     let newSum = 0;
+    if (bigger < smaller)
+        {
+            [smaller, bigger] = [bigger, smaller];
+        }
     for (let i = bigger; i >= smaller; i--)
         {
             newSum += bigger;
@@ -8,7 +12,7 @@ const sumAll = function(smaller, bigger) {
     return newSum;
 };
 
-console.log(sumAll(1, 4));
+console.log(sumAll(4, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
