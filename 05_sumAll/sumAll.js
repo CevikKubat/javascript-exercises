70ba5e1881ad3486d821dd76284fbe741a2abd3a
Cevik <cev@hotmail.de>
2024-05-29 22:33:05 +0200

    change logic of the code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(smaller, bigger) {
+    let newSum = 0;
+    for (let i = bigger; i >= smaller; i--)
+        {
+            newSum += bigger;
+            bigger = bigger - 1;
+        }
+    return newSum;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
