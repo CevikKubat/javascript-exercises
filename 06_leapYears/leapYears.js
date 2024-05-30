4a2fc346406dfe2acd1fd9067d992f57d01f6dd3
Cevik <cev@hotmail.de>
2024-05-30 15:32:50 +0200

    determine leap year for both century and non-century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,21 @@
 const leapYears = function(year) {
-    if (year % 4 == 0 && !year % 100 == 0)
+    if (year % 400 == 0)
         {
             return true;
         }
+    else if (year % 100 == 0)
+        {
+            return false;
+        }
+    else if (year % 4 == 0)
+    {
+        return true;
+    }
     else
     {
         return false;
     }
 };
-
-console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
