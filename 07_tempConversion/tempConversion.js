1e149f6670f705932f48fed406a3630c4687b648
Cevik <cev@hotmail.de>
2024-05-30 15:40:16 +0200

    convert in temperatures in both directions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return (celsius * 9 / 5) + 32;
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
