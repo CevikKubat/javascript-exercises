8807e7f9d578a4a56302e79d823846ca7e485a4f
Cevik <cev@hotmail.de>
2024-07-23 22:36:04 +0200

    solve problems

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedArray = string.toLowerCase().split("").filter(element => element != "!" && element != "." && element != "?" && element != "," && element != " ");
+    let cleanedString = cleanedArray.join("");
+    let reversedString = cleanedArray.reverse().join("");
+    console.log("cleanedString: " + cleanedString);
+    console.log("reversedString: " + reversedString);
+    if (reversedString == cleanedString) return true;
+    else return false;
 };
-
+console.log(palindromes("A car, a man, a maraca."))
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0){
+        return "OOPS";
+    }
+    if (typeof number == "string"){
+        number = Number(number);
+    }
+    if (number <= 1){
+        return number;
+    }
+    else{
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 };
 
+console.log(fibonacci(15));
 // Do not edit below this line
 module.exports = fibonacci;
