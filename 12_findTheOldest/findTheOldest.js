91e4059c4726819126fe19605dfb413c606221bf
Cevik <cev@hotmail.de>
2024-07-27 14:46:46 +0200

    solve problem

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,35 +1,18 @@
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1066,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  },
-]
-
-const findTheOldest = function(people) {
-    let age = 0;
-    for (let i = 0; i < people.length; i++){
-      age = people[i].yearOfDeath - people[i].yearOfBirth;
-      if (isNaN(age)){
-        people[i].age = 0;
-      }
-      else {
-        people[i].age = age;
-      }
-      }
-    people.sort((a, b) => a.age > b.age ? -1 : 1);
-    return people[0];
-};
-console.log(findTheOldest(people))
+const findTheOldest = function(people){
+  let age = 0;
+  let dead = false;
+  for (let i = 0; i < people.length; i++){
+    if ('yearOfDeath' in people[i]){
+      age = people[i].yearOfDeath - people[i].yearOfBirth; 
+    }
+    else {
+      age = 2024 - people[i].yearOfBirth;
+    }
+    people[i].age = age;
+  }
+  people.sort((a, b) => a.age > b.age ? -1 : 1);
+  return people[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
